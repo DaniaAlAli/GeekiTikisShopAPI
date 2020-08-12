246e24f1c6ed258fd95bf4ede67d03d8ba354f66
DaniaAlAli <dania.al-ali@hotmail.com>
2020-08-12 11:26:42 +0300

    add user model

--- db/models/index.js
+++ db/models/index.js
@@ -1,10 +1,14 @@
 const Vendor = require("./Vendor");
 const Mug = require("./Mug");
+const User = require("./User");
 
 //A Vendor has Many mugs
-Vendor.hasMany(Mug, { as: "mugs", foreignKey: "vendorId", allowNull: false }); //lowercase d for ID
+Vendor.hasMany(Mug, { as: "mugs", foreignKey: "vendorId", allowNull: false });
+
 Mug.belongsTo(Vendor, { as: "vendor" });
+
 module.exports = {
   Vendor,
   Mug,
+  User,
 };
